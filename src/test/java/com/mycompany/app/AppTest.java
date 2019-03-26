8d98aaea79942688457bb0bc74b88f1b9ebba7cc
Ahmer Saeed <ahmer.saeed@skillorbit.com>
2019-03-26 17:06:55 +0500

    committing
    testing 1

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -34,9 +34,9 @@ public class AppTest
     {
         App.main(null);
         try {
-            assertEquals("Hello World!" + System.getProperty("line.separator"), outContent.toString());
+            assertEquals("222 - This is sample NBO - 222" + System.getProperty("line.separator"), outContent.toString());
         } catch (AssertionError e) {
-            fail("\"message\" is not \"Hello World!\"");
+            fail("\"message\" is not \"222 - This is sample NBO - 222\"");
         }
     }
 
